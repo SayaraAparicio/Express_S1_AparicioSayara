11fd518c8bad131cb94b90b2f2dd5f063096dbea
SayaraAparicio <sayara.arciniegas@gmail.com>
2025-09-14 21:57:15 -0500

    feat: :fire: dia5

---
+++ Dia5/models/userModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const UserSchema=new mongoose.Schema ({
+    name:{type:String,required:true,trim:true},
+    email:{type:String,required:true,lowercase:true,unique:true,trim:true},
+    age: {type:Number,min:0}
+}, {timestamps:true});
+
+/*
+-- Clase de dominio --
+*/
+
+class UserClass{
+    get isAdult(){
+        return (this.age ?? 0)>=18;
+    }
+    static async findByEmail(email){
+        return this.findOne({email});
+    }
+}
+UserSchema.loadClass(UserClass);
+export const UserModel=mongoose.model("User",UserSchema);
